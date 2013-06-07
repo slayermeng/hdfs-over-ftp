84bc2f9bb0283fcb56de97a4472c1196b6103ca6
slayermeng <mengxin83@gmail.com>
2013-06-07 15:09:52 +0800

    modify HdfsFileSystemManager can enter into user homedirectory

--- src/main/java/org/apache/hadoop/contrib/ftp/HdfsFileSystemManager.java
+++ src/main/java/org/apache/hadoop/contrib/ftp/HdfsFileSystemManager.java
@@ -1,15 +1,23 @@
 package org.apache.hadoop.contrib.ftp;
 
+import java.io.File;
+
 import org.apache.ftpserver.ftplet.FileSystemManager;
 import org.apache.ftpserver.ftplet.FileSystemView;
 import org.apache.ftpserver.ftplet.FtpException;
 import org.apache.ftpserver.ftplet.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Impelented FileSystemManager to use HdfsFileSystemView
  */
 public class HdfsFileSystemManager implements FileSystemManager {
 	public FileSystemView createFileSystemView(User user) throws FtpException {
-		return new HdfsFileSystemView(user);
+		// create home if does not exist
+		String homeDirStr = user.getHomeDirectory();
+		FileSystemView view = new HdfsFileSystemView(user);
+		view.changeDirectory(homeDirStr);
+		return view;
 	}
 }
